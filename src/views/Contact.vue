f9a32a268ba8dce45e3860297f9829799b9502c9
Ashan R <ashan.rajapaksha@qualitapps.com>
2025-12-10 09:42:07 +0530

    initial commit vue 3.5 app

---
+++ src/views/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Contact Page</h1>
+    <p>Get in touch with us!</p>
+    <form>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name"><br><br>
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email"><br><br>
+      <label for="message">Message:</label>
+      <textarea id="message" name="message"></textarea><br><br>
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to Vue 3.5.25 with Router!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
